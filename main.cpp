af7583badb2fbfdd8a71f65c1c0b09a6d71b6f7c
MatheusBonavite <matheusbonavite@gmail.com>
2024-09-23 23:14:20 -0300

    Centralizing some code in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
-#include <string>
 #include <expected>
 #include <SDL.h>
 #include "./utils/ErrorChecker.h"
+#include "./utils/Vec2D.h"
 
+struct RGBAlpha { uint8_t red; uint8_t green; uint8_t blue; uint8_t alpha; };
 struct BasicConfig { int x{0}; int y{0}; int width{800}; int height{800}; };
 static BasicConfig basicConfig;
 static int quitApplication = 0;
@@ -27,6 +27,12 @@ private:
 	uint32_t m_frameStart;
 	uint32_t m_frameDelay{100};
 };
+
+void SDLRenderLine(SDL_Renderer* renderer, Vec2D<int> startPoint, Vec2D<int> endPoint, RGBAlpha rgb)
+{
+	ErrorChecker::exitOnUnexpected(SDL_SetRenderDrawColor(renderer, rgb.red, rgb.green, rgb.blue, rgb.alpha));
+	ErrorChecker::exitOnUnexpected(SDL_RenderDrawLine(renderer, startPoint.x, startPoint.y, endPoint.x, endPoint.y));
+}
 
 int main(int argc, char* argv[])
 {
@@ -55,10 +61,8 @@ int main(int argc, char* argv[])
 		ErrorChecker::exitOnUnexpected(SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255));
 		ErrorChecker::exitOnUnexpected(SDL_RenderClear(renderer));
 		// Drawing a random line... To see if it is possible
-		ErrorChecker::exitOnUnexpected(SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255));
-		ErrorChecker::exitOnUnexpected(SDL_RenderDrawLine(renderer, 0, 0, basicConfig.width, basicConfig.height));
-		ErrorChecker::exitOnUnexpected(SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255));
-		ErrorChecker::exitOnUnexpected(SDL_RenderDrawLine(renderer, basicConfig.width, 0, 0, basicConfig.height));
+		SDLRenderLine(renderer, Vec2D<int>(0, 0), Vec2D<int>(basicConfig.width, basicConfig.height), RGBAlpha{255, 0, 0, 255});
+		SDLRenderLine(renderer, Vec2D<int>(basicConfig.width, 0), Vec2D<int>(0, basicConfig.height), RGBAlpha{255, 0, 0, 255});
 		// Let us now draw a random Rectangle
 		ErrorChecker::exitOnUnexpected(SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255));
 		SDL_Rect rect { (basicConfig.width / 2) - 8, (basicConfig.height / 2) - 8, 16, 16 };
